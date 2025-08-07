5fce58bdc85321eed44bfea056a2ea3af2e65d84
Nisha <nagdevenisha@gmail.com>
2025-08-07 15:27:39 +0530

    backend update

--- client/RedisClient.js
+++ client/RedisClient.js
@@ -9,3 +9,10 @@ import Redis from "ioredis";
 export const redis = new Redis(
  process.env.REDIS_URL
 );
+redis.on("connect", () => {
+  console.log("✅ Connected to Redis");
+});
+
+redis.on("error", (err) => {
+  console.error("❌ Redis connection error:", err);
+});
