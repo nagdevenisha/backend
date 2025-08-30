0b6b411b4ac40299b1ad0f51ee4fc32d1a379cfc
Nisha <nagdevenisha@gmail.com>
2025-08-30 14:38:01 +0530

    changes

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 import { prisma } from './client/PrismaClients.js';
 import { redis } from './client/RedisClient.js';
 import express from "express";
-<<<<<<< HEAD
+
 import { execFile ,exec} from 'child_process';
-=======
+
 import { execFile,exec } from 'child_process';
->>>>>>> 33d0d2d (changes defined)
+
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import cors from 'cors';
