16bd7905e584b25cba8fb46c792cd1e88f71e52b
Nisha <nagdevenisha@gmail.com>
2025-08-30 17:35:30 +0530

    do

--- Server/uploadFileToS3.js
+++ Server/uploadFileToS3.js
@@ -1,28 +1,37 @@
-// Server/s3.js
-const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
+const { S3Client, PutObjectCommand, GetObjectCommand } = require("@aws-sdk/client-s3");
+const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
+const fs = require("fs");
 
 const s3 = new S3Client({
-  region: process.env.AWS_REGION, // e.g. "ap-south-1"
+  region: process.env.AWS_REGION,
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   },
 });
 
+// Upload file to S3
 async function uploadFileToS3(bucket, key, filePath) {
-  const fs = require("fs");
   const fileStream = fs.createReadStream(filePath);
 
   const command = new PutObjectCommand({
     Bucket: bucket,
-    Key: key,          // e.g. "audio/merged.mp3"
+    Key: key,
     Body: fileStream,
-    ContentType: "audio/mpeg",
+    ContentType: "audio/mpeg", // for JSON you can use "application/json"
   });
 
   await s3.send(command);
 
-  return `https://${bucket}.s3.${process.env.AWS_REGION}.amazonaws.com/${key}`;
+  // Return pre-signed URL valid for 1 hour
+  return await getPresignedUrl(bucket, key, 3600);
 }
 
-module.exports = { uploadFileToS3 };
+// Generate pre-signed URL for an existing file
+async function getPresignedUrl(bucket, key, expiresIn = 3600) {
+  const command = new GetObjectCommand({ Bucket: bucket, Key: key });
+  const url = await getSignedUrl(s3, command, { expiresIn });
+  return url;
+}
+
+module.exports = { uploadFileToS3, getPresignedUrl };
